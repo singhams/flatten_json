f996a430c6873b16413c3f3dd4d0264d43e2f5f5
singhams <88553914+singhams@users.noreply.github.com>
2024-10-31 23:07:15 -0700

    Update flatten_json.py

--- flatten_json.py
+++ flatten_json.py
@@ -3,19 +3,30 @@ import pandas as pd
 import json
 from io import BytesIO
 
+# Function to preprocess JSON keys
+def preprocess_keys(obj, delimiter, replacement):
+    if isinstance(obj, dict):
+        new_obj = {}
+        for k, v in obj.items():
+            new_key = k.replace(delimiter, replacement)
+            new_obj[new_key] = preprocess_keys(v, delimiter, replacement)
+        return new_obj
+    elif isinstance(obj, list):
+        return [preprocess_keys(item, delimiter, replacement) for item in obj]
+    else:
+        return obj
+
 # Function to flatten a JSON object
 def flatten_json(nested_json, delimiter):
     out = {}
 
     def flatten(x, name=''):
-        if type(x) is dict:
+        if isinstance(x, dict):
             for a in x:
-                flatten(x[a], name + a + delimiter)
-        elif type(x) is list:
-            i = 0
-            for a in x:
-                flatten(a, name + str(i) + delimiter)
-                i += 1
+                flatten(x[a], f"{name}{a}{delimiter}")
+        elif isinstance(x, list):
+            for i, a in enumerate(x):
+                flatten(a, f"{name}{i}{delimiter}")
         else:
             out[name[:-len(delimiter)]] = x
 
@@ -31,21 +42,28 @@ uploaded_file = st.file_uploader("Choose a JSON file", type="json")
 # Input for delimiter
 delimiter = st.text_input("Enter delimiter", value="_")
 
+# Input for replacement character
+replacement = st.text_input("Enter replacement character for delimiter conflicts", value="-")
+
 # Button to process the file
 if st.button("Convert to Excel"):
     if uploaded_file is not None:
         # Load the JSON file
         data = json.load(uploaded_file)
 
+        # Preprocess the JSON object to replace instances of the delimiter
+        preprocessed_data = preprocess_keys(data, delimiter, replacement)
+
         # Flatten the JSON object
-        flat = flatten_json(data, delimiter)
+        flat = flatten_json(preprocessed_data, delimiter)
 
         # Convert the flattened JSON object into a DataFrame
         df = pd.DataFrame(list(flat.items()), columns=['Key', 'Value'])
 
         # Convert DataFrame to Excel
         output = BytesIO()
-        df.to_excel(output, index=False)
+        with pd.ExcelWriter(output, engine='xlsxwriter') as writer:
+            df.to_excel(writer, index=False)
         output.seek(0)
 
         # Provide download link for the Excel file
